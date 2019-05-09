8e376ac98ceae7e094f815b2ab09e78a9df75fea
salEZY <bobicaleks@yahoo.com>
2019-05-09 21:02:42 +0200

    add initial get req

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
+const express = require('express');
+
+const spomenici = require('./routes/spomenici');
 
 const URI = "mongodb://skolakoda:skolakoda523@ds111078.mlab.com:11078/heroku_tvw5zpg7";
+
+const app = express();
 
 MongoClient.connect(URI, (err, db) => {
     if (err) throw err;
@@ -25,3 +30,4 @@ MongoClient.connect(URI, (err, db) => {
     })
     db.close()
 })
+app.get('/spomenici', spomenici);
---
+++ routes/spomenici.js
@@ -0,0 +1,8 @@
+const MongoClient = require("mongodb").MongoClient;
+const URI = "mongodb://skolakoda:skolakoda523@ds111078.mlab.com:11078/heroku_tvw5zpg7";
+
+const spomenici = (req, res) => {
+
+}
+
+module.exports = filmovi
