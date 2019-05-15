c8f847ac2fcfbe50867d072833369ab8bafd6c38
Aleksandar Bobic <bobicaleks@yahoo.com>
2019-05-15 13:10:02 +0200

    Update setup.js

--- config/setup.js
+++ config/setup.js
@@ -1,7 +1,7 @@
 const port = process.env.PORT  || 8080;
 const URI = process.env.DB_URI;
 
-const domain = process.env.NODE_ENV == 'development'
+const domain = process.env.NODE_ENV === 'development'
   ? `http://localhost:${port}`
   : 'https://spomenici-api.herokuapp.com/'
 
