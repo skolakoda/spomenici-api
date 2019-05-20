5642b414fc6eeeebb31cf72b173b01315ca0ddee
Emil Bisak <bisak.emil@gmail.com>
2019-05-20 19:45:23 +0200

    create POST request, add new location

--- routes/dodaj.js
+++ routes/dodaj.js
@@ -1,9 +1,35 @@
 const mongo = require("mongodb");
-
 const { URI, DB_NAME } = require('../config/setup');
 
+
+
 const dodaj = (req, res) => {
-    res.send('bla');
+    const { naslov, kategorija, lat, lon } = req.body;
+
+    if (!naslov || !kategorija) {
+        res.send("Niste uneli sva potrebna polja")
+    }
+
+    mongo.MongoClient.connect(URI, (err, db) => {
+        if (err) throw err;
+        let mydb = db.db(DB_NAME);
+        let model = {
+            naslov, kategorija, "lokacija": {
+                lat, lon
+            }
+        };
+
+
+        console.log("RES", res);
+
+
+        mydb.collection('spomenici').insertOne(model, (err, res) => {
+            if (err) throw err;
+            db.close();
+        })
+        db.close()
+        res.send(`Dodat Spomenik ${naslov}`)
+    })
 }
 
 module.exports = dodaj
