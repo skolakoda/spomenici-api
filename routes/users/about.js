1d0e6a755deaf2417e564c210e020289448dc987
salEZY <bobicaleks@yahoo.com>
2019-06-03 10:26:20 +0200

    add user about route

---
+++ routes/users/about.js
@@ -0,0 +1,22 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+const { URI, DB_NAME } = require("../../config/setup");
+
+const about = (req, res) => {
+  const { id } = req.params;
+
+  MongoClient.connect(URI, { useNewUrlParser: true }, (err, db) => {
+    if (err) throw err;
+    if (!ObjectID.isValid(id)) return res.status(400).send("Nije validan id.");
+
+    db.db(DB_NAME)
+      .collection("korisnici")
+      .findOne({ _id: ObjectID(id) }, (err, user) => {
+        if (err) throw err;
+        res.send(`Pozdrav ${user.email}!. Tvoj ID => ${user._id}`); // trenutno samo
+      });
+    db.close();
+  });
+};
+
+module.exports = about;
