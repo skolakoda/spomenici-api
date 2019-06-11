d7075504f01ca73c7b6d3d6d14779e19ea5e7111
salEZY <bobicaleks@yahoo.com>
2019-06-11 21:21:53 +0200

    semicolon fix

--- routes/kolekcije/obrisi.js
+++ routes/kolekcije/obrisi.js
@@ -1,31 +1,31 @@
-const { MongoClient, ObjectID } = require("mongodb");
-const jwt = require("jsonwebtoken");
+const { MongoClient, ObjectID } = require("mongodb")
+const jwt = require("jsonwebtoken")
 
-const { URI, DB_NAME, tokenKey } = require("../../config/setup");
-const { ErrRes, SuccRes } = require("../../utils/interfaces");
+const { URI, DB_NAME, tokenKey } = require("../../config/setup")
+const { ErrRes, SuccRes } = require("../../utils/interfaces")
 
 const obrisi = (req, res) => {
   jwt.verify(req.token, tokenKey, err => {
     if (err) {
-      return res.status(403).send(new ErrRes("Pogresan token"));
+      return res.status(403).send(new ErrRes("Pogresan token"))
     }
-    const { kolekcija, id } = req.params;
+    const { kolekcija, id } = req.params
 
     if (!ObjectID.isValid(id)) {
-      return res.status(400).send(new ErrRes("Nije validan id."));
+      return res.status(400).send(new ErrRes("Nije validan id."))
     }
 
     MongoClient.connect(URI, { useNewUrlParser: true }, (err, db) => {
-      if (err) throw err;
+      if (err) throw err
       db.db(DB_NAME)
         .collection(kolekcija)
         .deleteOne({ _id: ObjectID(id) })
         .then(result =>
           res.send(new SuccRes(`Obrisano ${result.deletedCount} lokacija.`))
         )
-        .catch(err => res.status(500).send(new ErrRes(`Greska: ${err}`)));
-    });
-  });
-};
+        .catch(err => res.status(500).send(new ErrRes(`Greska: ${err}`)))
+    })
+  })
+}
 
-module.exports = obrisi;
+module.exports = obrisi
--- routes/users/login.js
+++ routes/users/login.js
@@ -1,40 +1,38 @@
-const { MongoClient } = require("mongodb");
-const md5 = require("md5");
-const jwt = require("jsonwebtoken");
+const { MongoClient } = require("mongodb")
+const md5 = require("md5")
+const jwt = require("jsonwebtoken")
 
-const { URI, DB_NAME, tokenKey } = require("../../config/setup");
-const { ErrRes } = require("../../utils/interfaces");
+const { URI, DB_NAME, tokenKey } = require("../../config/setup")
+const { ErrRes, SuccRes } = require("../../utils/interfaces")
 
 const login = (req, res) => {
-  const { email, password } = req.body;
-  const pw = md5(password);
+  const { email, password } = req.body
+  const pw = md5(password)
   MongoClient.connect(URI, { useNewUrlParser: true }, (err, db) => {
-    if (err) throw err;
+    if (err) throw err
     db.db(DB_NAME)
       .collection("korisnici")
       .findOne({ email, pass: pw }, (err, user) => {
         if (!user) {
-          return res
-            .status(400)
-            .send(new ErrRes("Pogresan email ili password"));
+          return res.status(400).send(new ErrRes("Pogresan email ili password"))
         }
-        const token = jwt.sign({ user }, tokenKey, { expiresIn: "30d" });
-        res.json(new SuccRes("Success! Token sent", token));
-        const date = Date();
+        const token = jwt.sign({ user }, tokenKey, { expiresIn: "30d" })
+        res.json(new SuccRes("Success! Token sent", token))
+        const date = Date()
         const tokenModel = {
           userId: user._id,
           token,
           dodat: date.toString()
-        };
+        }
 
         db.db(DB_NAME)
           .collection("tokens")
           .insertOne(tokenModel, err => {
-            if (err) throw err;
-            console.log(`Dodato u ${tokenModel.dodat}`);
-          });
-      });
-  });
-};
+            if (err) throw err
+            console.log(`Dodato u ${tokenModel.dodat}`)
+          })
+      })
+  })
+}
 
-module.exports = login;
+module.exports = login
