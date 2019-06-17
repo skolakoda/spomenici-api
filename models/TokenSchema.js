4ab6e05c799908ff54a26a5a6cc7c097434be782
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-17 11:17:17 +0200

    implement document reference

--- models/TokenSchema.js
+++ models/TokenSchema.js
@@ -13,7 +13,8 @@ const TokenSchema = new mongoose.Schema({
     required: true
   },
   userId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true
   }
 })
