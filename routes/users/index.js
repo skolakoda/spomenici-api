f0927d830ada0e999b8cc824467468ee9c737d4c
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-17 10:33:25 +0200

    cleanup user routes index

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,12 +3,8 @@ const router = express.Router()
 
 const { tokenCheck } = require('../../utils/helpers')
 
-const about = require('./about')
-const registracija = require('./registracija')
-const login = require('./login')
-
-router.post('/login', login)
-router.get('/pokazi/:id', tokenCheck, about)
-router.post('/registracija', registracija)
+router.post('/login', require('./login'))
+router.post('/registracija', require('./registracija'))
+router.get('/pokazi/:id', tokenCheck, require('./about'))
 
 module.exports = router
