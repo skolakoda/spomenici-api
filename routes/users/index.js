e7cf8d07cc8d14f04aa65aa2c2d6ccdb4cf7dd19
Aleksandar Bobic <bobicaleks@yahoo.com>
2019-06-20 14:51:12 +0200

    Update index.js

--- routes/users/index.js
+++ routes/users/index.js
@@ -5,8 +5,8 @@ const { tokenCheck } = require('../../utils/helpers')
 
 router.post('/login', require('./login'))
 router.post('/registracija', require('./registracija'))
-router.use(tokenCheck)
 router.get('/email', require('./email'))
+router.use(tokenCheck)
 router.get('/pokazi/:id', require('./about'))
 router.put('/uredi/:id', require('./uredi'))
 
