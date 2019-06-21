aa08894959663906072bb277574636d40605cd96
salEZY <bobicaleks@yahoo.com>
2019-06-21 11:35:21 +0200

    fix email refactor

--- utils/helpers.js
+++ utils/helpers.js
@@ -25,30 +25,30 @@ const tokenCheck = (req, res, next) => {
 }
 
 const sendEmail = (req, res, email) => {
-      const transporter = nodeMailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: 'spomeniciskolakoda@gmail.com',
-        pass: `${emailPass}`
-      }
-    })
-    const trialPass = `${Math.floor(Math.random() * 10000000)}`
-    
-    const mailOptions = {
-      from: 'spomeniciskolakoda@gmail.com',
-      to: email,
-      subject: 'Promena sifre!',
-      text: `Vasa trenutna sifra je ${trialPass} . Molimo da je promenite na sajtu!`
+  const transporter = nodeMailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'spomeniciskolakoda@gmail.com',
+      pass: `${emailPass}`
     }
-  
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        res.status(400).send(new ErrRes(err.message))
-      } else {
-        res.status(200).send(new SuccRes(`Email je poslat! ${info.response}`))
-      }
-    })
-    return trialPass
+  })
+  const trialPass = `${Math.floor(Math.random() * 10000000)}`
+
+  const mailOptions = {
+    from: 'spomeniciskolakoda@gmail.com',
+    to: email,
+    subject: 'Promena sifre!',
+    text: `Vasa trenutna sifra je ${trialPass} . Molimo da je promenite na sajtu!`
+  }
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.status(400).send(new ErrRes(err.message))
+    } else {
+      res.status(200).send(new SuccRes(`Email je poslat! ${info.response}`))
+    }
+  })
+  return trialPass
 }
 
 module.exports = {
