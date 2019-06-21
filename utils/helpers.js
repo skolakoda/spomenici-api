e8bde16f846bb34f0693efd648e6614f763add74
Aleksandar Bobic <bobicaleks@yahoo.com>
2019-06-21 08:39:33 +0200

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken')
+const nodeMailer = require('nodemailer')
 
-const { tokenKey } = require('./config')
-const { ErrRes } = require('./interfaces')
+const { tokenKey, emailPass } = require('./config')
+const { SuccRes, ErrRes } = require('./interfaces')
 
 const nevalidnaLokacija = (lat, lon) =>
   lat > 47.2 || lat < 42 || lon > 23 || lon < 19
@@ -23,8 +24,35 @@ const tokenCheck = (req, res, next) => {
   }
 }
 
+const sendEmail = (email) => {
+      const transporter = nodeMailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'spomeniciskolakoda@gmail.com',
+        pass: `${emailPass}`
+      }
+    })
+  
+    const mailOptions = {
+      from: 'spomeniciskolakoda@gmail.com',
+      to: email,
+      subject: 'Promena sifre!',
+      text: `Vasa trenutna sifra je ${trialPass} . Molimo da je promenite na sajtu!`
+    }
+  
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        res.status(400).send(new ErrRes(err.message))
+      } else {
+        res.status(200).send(new SuccRes(`Email je poslat! ${info.response}`))
+      }
+    })
+
+}
+
 module.exports = {
   nevalidnaLokacija,
   emailCheck,
-  tokenCheck
+  tokenCheck,
+  sendEmail
 }
