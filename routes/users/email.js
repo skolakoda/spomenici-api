22c9fb1d20e72a108df19283b4d5f1b1f83f3650
Aleksandar Bobic <bobicaleks@yahoo.com>
2019-06-21 08:41:16 +0200

    Update email.js

--- routes/users/email.js
+++ routes/users/email.js
@@ -1,8 +1,8 @@
-const nodeMailer = require('nodemailer')
 const md5 = require('md5')
 
 const { ErrRes, SuccRes } = require('../../utils/interfaces')
 const { emailPass } = require('../../utils/config')
+const { sendEmail } = require('../../utils/helpers')
 const User = require('../../models/User')
 
 const email = (req, res) => {
@@ -12,29 +12,10 @@ const email = (req, res) => {
     if (!user) {
       return res.send(new ErrRes('Email se ne nalazi u bazi korisnika'))
     }
+    
     const trialPass = `${Math.floor(Math.random() * 10000000)}`
-    const transporter = nodeMailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: 'spomeniciskolakoda@gmail.com',
-        pass: `${emailPass}`
-      }
-    })
-  
-    const mailOptions = {
-      from: 'spomeniciskolakoda@gmail.com',
-      to: email,
-      subject: 'Promena sifre!',
-      text: `Vasa trenutna sifra je ${trialPass} . Molimo da je promenite na sajtu!`
-    }
-  
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        res.status(400).send(new ErrRes(err.message))
-      } else {
-        res.status(200).send(new SuccRes(`Email je poslat! ${info.response}`))
-      }
-    })
+    
+    sendEmail(email)
 
     const password = md5(trialPass)
     user.password = password
