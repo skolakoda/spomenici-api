8b45db3560da99812e93f62b45ec1efa2b6f4b57
Emil Bisak <bisak.emil@gmail.com>
2019-06-24 19:46:36 +0200

    hotfix, promeni uredi rutu u PUT

--- routes/kolekcije/index.js
+++ routes/kolekcije/index.js
@@ -7,7 +7,7 @@ router.get('/:kolekcija/kategorije', require('./kategorije'))
 router.get('/:kolekcija/nadji/:id', require('./nadji'))
 router.use(tokenCheck)
 router.post('/:kolekcija/dodaj', require('./dodaj'))
-router.post('/:kolekcija/uredi/:id', require('./uredi'))
+router.put('/:kolekcija/uredi/:id', require('./uredi'))
 router.delete('/:kolekcija/obrisi/:id', require('./obrisi'))
 
 module.exports = router
