f078f39670fcfc2c82d3c744a1a7c1d587a1f8d6
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-28 16:30:59 +0200

    rename method

--- routes/kolekcije/index.js
+++ routes/kolekcije/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { tokenCheck, adminCheck } = require('../../utils/middleware')
+const { userCheck, adminCheck } = require('../../utils/middleware')
 
 router.get('/:kolekcija', require('./izlistaj'))
 router.get('/:kolekcija/kategorije', require('./kategorije'))
 router.get('/:kolekcija/nadji/:id', require('./nadji'))
-router.use(tokenCheck)
+router.use(userCheck)
 router.post('/:kolekcija/dodaj', require('./dodaj'))
 router.put('/:kolekcija/uredi/:id', require('./uredi'))
 router.use(adminCheck)
