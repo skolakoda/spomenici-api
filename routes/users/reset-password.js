13fe0aa193738fbf4b8380835949708f57c21d4b
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-28 16:44:41 +0200

    ukinut jedan nivo ugnjezdenja

--- routes/users/reset-password.js
+++ routes/users/reset-password.js
@@ -4,22 +4,19 @@ const { ErrRes, SuccRes } = require('../../utils/interfaces')
 const { sendEmail } = require('../../utils/helpers')
 const User = require('../../models/User')
 
-module.exports = (req, res) => {
-  const { email } = req.body
+module.exports = async(req, res) => {
+  const user = User.findOne({ email: req.body.email })
+  if (!user) return res.send(new ErrRes('Email se ne nalazi u bazi.'))
 
-  User.findOne({ email }).then(user => {
-    if (!user) return res.send(new ErrRes('Email se ne nalazi u bazi.'))
+  const newPass = Math.floor(Math.random() * 10000000)
+  sendEmail(user.email, 'reset', newPass)
+  user.password = md5(newPass)
 
-    const tempPass = Math.floor(Math.random() * 10000000)
-    sendEmail(user.email, 'reset', tempPass)
-    user.password = md5(tempPass)
-
-    user.save()
-      .then(data =>
-        res.json(new SuccRes(`Proverite email ${data.email} za novu lozinku.`))
-      )
-      .catch(err =>
-        res.status(400).send(`Greska : ${new ErrRes(err.message)}`)
-      )
-  })
+  user.save()
+    .then(data =>
+      res.json(new SuccRes(`Proverite email ${data.email} za novu lozinku.`))
+    )
+    .catch(err =>
+      res.status(400).send(`Greska : ${new ErrRes(err.message)}`)
+    )
 }
