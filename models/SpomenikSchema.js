fb1457f170ca9dd7c61b2c4c8443d2e0811c114a
Damjan Pavlica <mudroljub@gmail.com>
2019-07-19 23:25:25 +0200

    dodato imaSliku u shemu

--- models/SpomenikSchema.js
+++ models/SpomenikSchema.js
@@ -36,6 +36,10 @@ const SpomenikSchema = new mongoose.Schema({
     }
   },
   slika: String,
+  imaSliku: {
+    type: Boolean,
+    default() { return Boolean(this.slika) }
+  },
   website: {
     type: String,
     match: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm
